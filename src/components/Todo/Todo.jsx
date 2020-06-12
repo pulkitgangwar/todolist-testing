ede13ab03d2a604db1470b28119f45b32b138b06
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-12 11:40:08 +0530

    todo now has internal state fir editing a todo ,toggleeditstate function created to toggle a todo,edit todo will appear when edit button is clicked

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = ({ todo, removeTodo }) => {
+// importing components
+import EditTodo from "../EditTodo/EditTodo";
+
+const Todo = ({ todo, removeTodo, saveEditTodo }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const { id, text } = todo;
 
   const handleRemove = (e) => {
@@ -9,13 +14,31 @@ const Todo = ({ todo, removeTodo }) => {
     removeTodo(id);
   };
 
+  const toggleEditState = () => {
+    setIsEditing(!isEditing);
+  };
+
+  if (isEditing) {
+    return (
+      <div className="todo__edit">
+        <EditTodo
+          todo={todo}
+          saveEditTodo={saveEditTodo}
+          toggleEditState={toggleEditState}
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="todo" id={id}>
       <h1 className="todo__text">{text}</h1>
       <button className="todo__remove" onClick={handleRemove}>
         remove
       </button>
-      <button className="todo__edit">edit</button>
+      <button className="todo__edit" onClick={toggleEditState}>
+        edit
+      </button>
     </div>
   );
 };
