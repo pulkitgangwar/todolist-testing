1f9135e1685715e530e00915ae48e0aa9d5e5279
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-12 11:03:09 +0530

    using function removetodo to remvoe the todo

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, removeTodo }) => {
   const { id, text } = todo;
+
+  const handleRemove = (e) => {
+    const id = e.target.parentElement.id;
+
+    removeTodo(id);
+  };
+
   return (
     <div className="todo" id={id}>
       <h1 className="todo__text">{text}</h1>
-      <button className="todo__remove">remove</button>
+      <button className="todo__remove" onClick={handleRemove}>
+        remove
+      </button>
       <button className="todo__edit">edit</button>
     </div>
   );
