515d90d7007d9eee5062a4c32baa7c9d230d036d
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-12 11:44:01 +0530

    passing saveedittodo function to todo component to save a edited todo

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -3,11 +3,18 @@ import React from "react";
 // importing components
 import Todo from "../Todo/Todo";
 
-const Todos = ({ todos,removeTodo }) => {
+const Todos = ({ todos, toggleEditState, saveEditTodo, removeTodo }) => {
   return (
     <div className="todos">
       {todos.length
-        ? todos.map((todo) => <Todo todo={todo} key={todo.id} removeTodo={removeTodo} />)
+        ? todos.map((todo) => (
+            <Todo
+              todo={todo}
+              key={todo.id}
+              saveEditTodo={saveEditTodo}
+              removeTodo={removeTodo}
+            />
+          ))
         : "Please Enter a todo"}
     </div>
   );
